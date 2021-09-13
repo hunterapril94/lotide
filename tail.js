d8f60a8a7fa5c666d00c336b482e1e243ef95d84
April Larsen <hunterapril94@gmail.com>
2021-09-13 22:13:15 +0000

    return all elements but the head

---
+++ tail.js
@@ -0,0 +1,17 @@
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(JSON.stringify(result), JSON.stringify(["Lighthouse", "Labs"]));
