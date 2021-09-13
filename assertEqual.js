8269b4c02546cffaf7ebda259b508a6bf4823c82
April Larsen <hunterapril94@gmail.com>
2021-09-13 21:51:30 +0000

    change to template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\u2714 Assertion Passed: " + actual + " === " + expected);
+    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\u274C Assertion Failed: " + actual + " !== " + expected);
+    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
