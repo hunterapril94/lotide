285aa93286cc8df4bd68ae52a1692ca8393f4485
April Larsen <hunterapril94@gmail.com>
2021-09-15 20:51:05 +0000

    function to count letters in a string

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let letterCount = {};
+  let lowCaseString = string.toLowerCase();
+  for (const element of lowCaseString) {
+    if (element !== " ") {
+      if (letterCount[element]) {
+        letterCount[element] += 1;
+      } else {
+        letterCount[element] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+let myName = "April Larsen";
+assertEqual(countLetters(myName).l, 2);
+console.log(countLetters(myName));
