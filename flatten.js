11d95be64d3ce201b7eb558bebec5653c21c981e
April Larsen <hunterapril94@gmail.com>
2021-09-15 00:00:11 +0000

    returns one array when nested arrays

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array) {
+  let flattened = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        flattened.push(array[i][j]);
+      }
+    } else {
+      flattened.push(array[i]);
+    }
+  }
+  return flattened;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
