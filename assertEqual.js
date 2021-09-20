4e5cf1b0db1065527370f77ba6cafd59ab5cada3
April Larsen <hunterapril94@gmail.com>
2021-09-20 19:06:32 +0000

    DRY head and assertequal functions

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,4 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("aaa", "aaa");
-assertEqual(1, 3);
+module.exports = assertEqual;
